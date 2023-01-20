ed6f547b41b12a3269959f2881c10fd3723d6036
Ludmila Gritskiv <grickivludmila@gmail.com>
2023-01-20 13:21:57 +0200

    Added simple tests

---
+++ tests/api/test_api.py
@@ -0,0 +1,59 @@
+#def test_check_math():
+    #assert 7 * 7 == 49
+
+
+#def test_check_78():
+    #assert 7 * 8 == 56
+
+
+#class User:
+
+    #def __init__(self) -> None:
+        #self.name = 'Ludmila'
+        #self.second_name = 'Grickiv'
+
+
+#user = User()   
+
+
+#def test_remove_name():
+    #user.name = ''
+    #assert user.name == ''
+
+
+#def test_name():
+    #assert user.name == 'Ludmila'
+
+
+#def test_second_name():
+    #assert user.second_name == 'Grickiv'
+
+
+
+
+import pytest
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = 'Ludmila'
+        self.second_name = 'Grickiv'
+
+
+@pytest.fixture
+def user():
+    yield User()
+
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name(user):
+    assert user.name == 'Ludmila'
+
+
+def test_second_name(user):
+    assert user.second_name == 'Grickiv'    
