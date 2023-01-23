d214651a55424fd3a764b6aff9f86cf6066b93c6
Ludmila Gritskiv <grickivludmila@gmail.com>
2023-01-23 12:16:25 +0200

    Modified the file ' tests/api/test_fixtures.py'

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -12,17 +12,19 @@ class User:
         self.name = ''  
         self.second_name = '' 
 
-    def test_change_name():
-        user = User()
-        user.create()
 
-        assert user.name == 'Ludmila'
-        user.remove()
+def test_change_name():
+    user = User()
+    user.create()
 
-    def test_change_second_name():
-        user = User()
-        user.create()
+    assert user.name == 'Ludmila'
+    user.remove()
 
-        assert user.second_name == 'Grickiv'
-        user.remove()    
+
+def test_change_second_name():
+    user = User()
+    user.create()
+
+    assert user.second_name == 'Grickiv'
+    user.remove()    
 
