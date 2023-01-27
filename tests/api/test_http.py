4880dfe0b6edb894ff1f9ca17eb8ff14150a51e1
Ludmila Gritskiv <grickivludmila@gmail.com>
2023-01-27 16:20:38 +0200

    Modified the file 'tests/api/test_http.py'

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -10,5 +10,8 @@ def test_first_requests():
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(r.text.decode("utf-8"))
+    #print(f"Response is {r.text}")
+    print(f"Response Body is {r.json()}")
+    print(f"Response Status code is {r.status_code}")
+    print(f"Response Headers are {r.headers}")
 
