c6ef0cd9234fd0535418cb3a2fe792a746f6b268
Ludmila Gritskiv <grickivludmila@gmail.com>
2023-01-27 17:16:54 +0200

    Prepared the file ' tests/api/test_http.py' for submission to GitHub

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -5,7 +5,7 @@ import requests
 @pytest.mark.http
 def test_first_requests():
     r = requests.get('https://api.github.com/zen')
-    print(r.text)
+    print(f"Response is {r.text}")
 
 @pytest.mark.http
 def test_second_request():
